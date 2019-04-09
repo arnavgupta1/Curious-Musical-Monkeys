f91c6a052ebbe91be11a52f0d71d7ea73b761483
amfaller <49420290+amfaller@users.noreply.github.com>
2019-04-09 18:35:56 -0400

    Test Button
    Added a simple test button and a simple proof-of-concept test function. When you click the button, the text goes from "Click me" to "Clicked"

--- app/src/main/java/com/example/curiousmusicalmonkeys/MainActivity.java
+++ app/src/main/java/com/example/curiousmusicalmonkeys/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.curiousmusicalmonkeys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Make a test button
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //testButton will correspond to testButton in activity_main.xml
+        testButton = findViewById(R.id.testButton);
+
+        testButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ((Button) v).setText("Clicked");
     }
 }
+
+
