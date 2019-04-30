8a69c2bc34e88acf895a347e5ce36becc6019e12
tottto45 <georgetks@gmail.com>
2019-04-30 00:10:14 -0400

    fuck bitches with veggies

--- app/src/main/java/com/example/curiousmusicalmonkeys/record.java
+++ app/src/main/java/com/example/curiousmusicalmonkeys/record.java
@@ -1,23 +1,45 @@
 package com.example.curiousmusicalmonkeys;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class record extends AppCompatActivity {
-    ImageButton Record;
+    ImageButton Record,Stup;
+    String Pathsbaby = "";
+    MediaRecorder micDrop;
+    final int REQUEST_PERMISION_CODE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
-        Record = findViewById(R.id.decord);
-       Record.setOnClickListener(new View.OnClickListener() {
+
+        if(checkPermissionFromDevice()){}
+
+        Record = findViewById(R.id.Rec);
+        Stup = findViewById(R.id.StopRec);
+        Record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
+       Stup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int write_external_storage_res = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_storage_res == PackageManager.PERMISSION_GRANTED && record_audio == PackageManager.PERMISSION_GRANTED;
     }
 }
